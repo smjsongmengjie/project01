c23e8e6e440df5dd607be7a9bf1ce9a5245b29be
Smj <smj@email.com>
2019-08-30 17:33:53 +0800

    Initial commit

---
+++ src/com/smj/test/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.smj.test;
+
+
+public class HelloWorld {
+
+    private String sname;
+
+    //模板一 psvm
+    //模板二 sout 变形soutp soutm soutv xxx.sout
+    //模板三 fori 变形iter itar
+    //模板四 lits.for 变形list.fori list.forr
+    //模板五 ifn inn xx.nn xx.null
+    //模板六 prsf psf psfi psfs
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("HelloWorld.main");
+        System.out.println("args = [" + args + "]");
+        System.out.println("args = " + args);
+        System.out.println("args = " + args);
+        System.out.println("args = [" + args + "]");
+    }
+
+    public void test(){
+        
+    }
+
+}
---
+++ src/com/smj/test/Too.java
@@ -0,0 +1,8 @@
+package com.smj.test;
+
+/**
+ * @author smj
+ * @create 2019/8/29 18:21
+ */
+public class Too {
+}
